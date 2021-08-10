21bde95256ec1023ba65c78ee207b5e718129a9e
Jenan Morrar <jenanmorrar06@gmail.com>
2021-08-10 15:24:52 +0300

    Adding_Generics_View

--- SchoolProject/SchoolApp/urls.py
+++ SchoolProject/SchoolApp/urls.py
@@ -10,5 +10,16 @@ router.register('classrooms',views.ClassroomView)
 router.register('grades',views.GradeView)
 
 urlpatterns = [
-    path('',include(router.urls)),
+    path('viewSets/',include(router.urls)),
+    path('generics/students/', views.StudentList.as_view()),
+    path('generics/students/<int:pk>', views.StudentDetail.as_view()),
+    path('generics/teachers/', views.TeachertList.as_view()),
+    path('generics/teachers/<int:pk>', views.TeachertDetail.as_view()),
+    path('generics/subjects/', views.SubjectList.as_view()),
+    path('generics/subjects/<int:pk>', views.SubjectDetail.as_view()),
+    path('generics/classrooms/', views.ClassroomList.as_view()),
+    path('generics/classrooms/<int:pk>', views.ClassroomDetail.as_view()),
+    path('generics/grades/', views.GradeList.as_view()),
+    path('generics/grades/<int:pk>', views.GradeDetail.as_view()),
+
 ]
--- SchoolProject/SchoolApp/views.py
+++ SchoolProject/SchoolApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets,generics
 from .models import Student,Teacher,Subject,ClassRoom,Grade
 from .serializers import StudentSerializer,TeacherSerializer,SubjectSerializer,ClassRoomSerializer,GradeSerializer
+
+# *************** using ViewSets *********************
 
 class StudentView(viewsets.ModelViewSet):
     queryset = Student.objects.all()
@@ -23,3 +25,46 @@ class GradeView(viewsets.ModelViewSet):
     queryset = Grade.objects.all()
     serializer_class = GradeSerializer
 
+# *************** using Generic *********************
+
+class StudentList(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class TeachertList(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class TeachertDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class SubjectList(generics.ListCreateAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class SubjectDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+
+class ClassroomList(generics.ListCreateAPIView):
+    queryset = ClassRoom.objects.all()
+    serializer_class = ClassRoomSerializer
+
+class ClassroomDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ClassRoom.objects.all()
+    serializer_class = ClassRoomSerializer
+
+
+class GradeList(generics.ListCreateAPIView):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
+
+class GradeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
